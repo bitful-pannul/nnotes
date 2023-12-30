f817849926fdb6278026393ec853e495c63a5c14
bitful-pannul <bitful-pancreas@protonmail.com>
2023-12-30 15:33:53 -0300

    add current file UX

--- ui/src/components/editor.tsx
+++ ui/src/components/editor.tsx
@@ -40,12 +40,31 @@ const TextEditor: React.FC<EditorProps> = ({ note, isOpen }) => {
       })
   };
 
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === 's') {
+        event.preventDefault();
+        handleSave();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [text]);
+
   if (!isOpen) {
     return null;
   }
 
+  // Get the last part of the path
+  const fileName = note.path.split('/').pop();
+
   return (
     <div>
+      <h3>{fileName}</h3>
       <textarea value={text} onChange={e => setText(e.target.value)} />
       <button onClick={handleSave}>Save</button>
       {saveMessage && <p>{saveMessage}</p>}
